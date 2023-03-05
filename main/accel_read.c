8fad9343a1c5bc6314df83b18385e6396875bdd7
Marshal Horn <kamocat@gmail.com>
2023-03-04 18:26:33 -0800

    Switched to mpu6050

--- main/accel_read.c
+++ main/accel_read.c
@@ -3,7 +3,7 @@
    Simple I2C example that shows how to initialize I2C
    as well as reading and writing from and to registers for a sensor connected over I2C.
 
-   The sensor used in this example is a ADXL345 inertial measurement unit.
+   The sensor used in this example is a MPU6050 inertial measurement unit.
 
    For other examples please check:
    https://github.com/espressif/esp-idf/tree/master/examples
@@ -17,6 +17,7 @@ Unless required by applicable law or agreed to in writing, this software is dist
 #include "esp_log.h"
 #include "driver/i2c.h"
 #include "esp_timer.h"
+#include "byteswap.h"
 
 static const char *TAG = "i2c";
 
@@ -29,34 +30,47 @@ static void esp_warn_check(esp_err_t err, int line){
 #define ESP_WARN(err) esp_warn_check(err, __builtin_LINE()) 
 
 #define I2C_MASTER_SCL_IO           19                        /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           18                        /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_SDA_IO           21                        /*!< GPIO number used for I2C master data  */
 #define I2C_MASTER_NUM              0                         /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
 #define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE   0                         /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE   0                         /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_TIMEOUT_MS       1000
 
-#define ADXL345_SENSOR_ADDR                    0x53        /*!< Slave address of the ADXL345 sensor */
-#define ADXL345_WHO_AM_I_REG_ADDR              0xE5        /*!< Device ID */
+#define MPU6050_SENSOR_ADDR                    0x68        /*!< Slave address of the MPU6050 sensor */
+#define MPU6050_WHO_AM_I_REG_ADDR              0x75        /*!< Device ID */
 /**
- * @brief Read a sequence of bytes from a ADXL345 sensor registers
+ * @brief Read a sequence of bytes from a MPU6050 sensor registers
  */
-static esp_err_t adxl_register_read(uint8_t reg_addr, uint8_t *data, size_t len)
+static esp_err_t register_read(uint8_t reg_addr, uint8_t *data, size_t len)
 {
-    return i2c_master_write_read_device(I2C_MASTER_NUM, ADXL345_SENSOR_ADDR, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    return i2c_master_write_read_device(I2C_MASTER_NUM, MPU6050_SENSOR_ADDR, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+}
+
+static esp_err_t register_write(uint8_t reg_addr, uint8_t * data, size_t len){
+    uint8_t * readback = (uint8_t *)malloc(len+1);
+    readback[0] = reg_addr;
+    for(size_t i = 0; i<len; ++i){ readback[i+1] = data[i];}
+    esp_err_t err = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6050_SENSOR_ADDR, readback, len+1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    if(ESP_OK == err){
+        err = register_read(reg_addr, readback, len);
+        if(ESP_OK == err){
+            for(size_t i = 0; i < len; ++i){
+                if(readback[i] != data[i])
+                    err = ESP_ERR_INVALID_RESPONSE;
+            }
+        }
+    }
+    free(readback);
+    return err;
 }
 
 /**
- * @brief Write a byte to a ADXL345 sensor register
+ * @brief Write a byte to a MPU6050 sensor register
  */
-static esp_err_t adxl_register_write_byte(uint8_t reg_addr, uint8_t data)
+static esp_err_t register_write_byte(uint8_t reg_addr, uint8_t data)
 {
-    int ret;
-    uint8_t write_buf[2] = {reg_addr, data};
-
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, ADXL345_SENSOR_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-
-    return ret;
+    return register_write(reg_addr, &data, 1);
 }
 
 /**
@@ -81,25 +95,19 @@ static esp_err_t i2c_master_init(void)
 }
 
 struct xyz{
-    float x;
-    float y;
-    float z;
+    int16_t x;
+    int16_t y;
+    int16_t z;
     float t;
 };
 
-static float unpack_bytes(int8_t lower_byte, int16_t upper_byte){
-    float x  = (upper_byte<<8)|lower_byte;
-    x /= 256;
-    return x;
-}
-
-static esp_err_t adxl_get_accel(struct xyz * data){
-    int8_t buf[6];
-    esp_err_t err = adxl_register_read(0x32, (uint8_t*)buf, 6);
-    //ESP_LOG_BUFFER_HEX(TAG, buf, 6);
-    data->x = unpack_bytes(buf[0], buf[1]);
-    data->y = unpack_bytes(buf[2], buf[3]);
-    data->z = unpack_bytes(buf[4], buf[5]);
+static esp_err_t get_accel(struct xyz * data){
+    int16_t buf[3];
+    esp_err_t err = register_read(0x3B, (uint8_t*)buf, 6);
+    //sESP_LOG_BUFFER_HEX(TAG, buf, 6);
+    data->x = __bswap_16(buf[0]);
+    data->y = __bswap_16(buf[1]);
+    data->z = __bswap_16(buf[2]);
     data->t = esp_timer_get_time() * 0.000001;
     return err;
 }
@@ -108,11 +116,12 @@ static esp_err_t adxl_get_accel(struct xyz * data){
 static struct xyz accel_log[ACCEL_LOG_SIZE];
 static size_t accel_latest;
 
-#define LAMBDA_COPY(val, stop) for(;i<stop;++i){written+=snprintf(dest+written,size-written,fmt,val);if((size-written)<min_size){return written;}}
+#define LAMBDA_COPY(val, stop, fmt) for(;i<stop;++i){written+=snprintf(dest+written,size-written,fmt,val);if((size-written)<min_size){return written;}}
 
 size_t accel_writer(char * dest, size_t size){
     const size_t min_size = 20; //Stop writing if not enough bytes are left
     const char * fmt = "%.3f,"; //Format floats to 3 digits past the decimal
+    const char * dfmt = "%d,"; //Integer format
     size_t written = 0;
 
     // All persistent variables are declared static
@@ -120,7 +129,7 @@ size_t accel_writer(char * dest, size_t size){
     static int iter = 0;
     switch(iter){ // Used to maintain state. Fall-through intended.
         case 0: // Setup
-            i1 = accel_latest + 2;
+            i1 = accel_latest + 10;
             i2 = ACCEL_LOG_SIZE;
             i3 = 0;
             i4 = accel_latest+1;
@@ -129,48 +138,48 @@ size_t accel_writer(char * dest, size_t size){
             __attribute__ ((fallthrough));
         case 1:
             iter = 1;
-            LAMBDA_COPY(accel_log[i].t, i2)
+            LAMBDA_COPY(accel_log[i].t, i2, fmt)
             i = i3;
             __attribute__ ((fallthrough));
         case 2:
             iter = 2;
-            LAMBDA_COPY(accel_log[i].t, i4)
+            LAMBDA_COPY(accel_log[i].t, i4, fmt)
             --written;
             written += snprintf(dest+written, size-written, "],["); // Array seperator
             i = i1;
             __attribute__ ((fallthrough));
         case 3:
             iter = 3;
-            LAMBDA_COPY(accel_log[i].x, i2)
+            LAMBDA_COPY(accel_log[i].x, i2, dfmt)
             i = i3;
             __attribute__ ((fallthrough));
         case 4:
             iter = 4;
-            LAMBDA_COPY(accel_log[i].x, i4)
+            LAMBDA_COPY(accel_log[i].x, i4, dfmt)
             --written;
             written += snprintf(dest+written, size-written, "],["); // Array seperator
             i = i1;
             __attribute__ ((fallthrough));
         case 5:
             iter = 5;
-            LAMBDA_COPY(accel_log[i].y, i2)
+            LAMBDA_COPY(accel_log[i].y, i2, dfmt)
             i = i3;
             __attribute__ ((fallthrough));
         case 6:
             iter = 6;
-            LAMBDA_COPY(accel_log[i].y, i4)
+            LAMBDA_COPY(accel_log[i].y, i4, dfmt)
             --written;
             written += snprintf(dest+written, size-written, "],["); // Array seperator
             i = i1;
             __attribute__ ((fallthrough));
         case 7:
             iter = 7;
-            LAMBDA_COPY(accel_log[i].z, i2)
+            LAMBDA_COPY(accel_log[i].z, i2, dfmt)
             i = i3;
             __attribute__ ((fallthrough));
         case 8:
             iter = 8;
-            LAMBDA_COPY(accel_log[i].z, i4)
+            LAMBDA_COPY(accel_log[i].z, i4, dfmt)
             --written;
             written += snprintf(dest+written, size-written, "]]"); // Array end
             iter = 9;
@@ -183,29 +192,53 @@ size_t accel_writer(char * dest, size_t size){
 
 void accel_reader_task(void *pvParameters)
 {
-    uint8_t data[2];
+    uint8_t data[128];
     struct xyz accel;
     accel_latest = 0;
     ESP_ERROR_CHECK(i2c_master_init());
     ESP_LOGI(TAG, "I2C initialized successfully");
 
-    /* Read the ADXL345 WHO_AM_I register, on power up the register should have the value 0xE5 */
-    ESP_WARN(adxl_register_read(ADXL345_WHO_AM_I_REG_ADDR, data, 1));
+    /* Read the MPU6050 WHO_AM_I register, should match the i2c addres */
+    ESP_WARN(register_read(MPU6050_WHO_AM_I_REG_ADDR, data, 1));
     ESP_LOGI(TAG, "WHO_AM_I = %X", data[0]);
 
     /* Configure the power control */
-    ESP_WARN(adxl_register_write_byte(0x2D, 0x08));
+    data[0] = 4; // FIFO reset
+    data[1] = 3; // Disable sleep. Use PLL from Z-axis gyroscope for more accurate clock
+    data[2] = 0;
+    ESP_WARN(register_write(0x6A, data, 3));
 
-    /* Configure data scaling */
-    ESP_WARN(adxl_register_write_byte(0x31, 0x0B));
+    /* Set the filter and sample rate */
+    data[0] = 9; // 100Hz sample rate
+    data[1] = 0x25; // 10Hz digital filter
+    data[2] = 0; // Disable Gyro self-test, set full-scale to 250 degrees/second
+    data[3] = 0; // Disable accel self-test, set full scale to 2G
+    ESP_WARN(register_write(0x19, data, 4));
 
+    ESP_WARN(register_write_byte(0x23,0x08)); // Set only Accelerometer to fill FIFO
+    ESP_WARN(register_write_byte(0x6A, 0x40));// Enable FIFO
+    accel.t = 0;
     while(1){
-        ESP_WARN(adxl_get_accel(&accel));
-        if( ++accel_latest >= ACCEL_LOG_SIZE)
-            accel_latest = 0;
-        accel_log[accel_latest] = accel;
-        ESP_LOGI(TAG, "%.3f %.3f %.3f %.3f %d", accel.x, accel.y, accel.z, accel.t, accel_latest);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        //ESP_WARN(get_accel(&accel));
+        int16_t fifo_size;
+        ESP_WARN(register_read(0x72,(uint8_t*)&fifo_size,2));
+        fifo_size = __bswap_16(fifo_size);
+        ESP_LOGI(TAG, "Fifo size: %d", fifo_size);
+        if(fifo_size > sizeof(data))
+            fifo_size = sizeof(data);
+        ESP_WARN(register_read(0x74,data,fifo_size));
+        int16_t * buf = (int16_t *)data;
+        for(;fifo_size>0;fifo_size -=6){
+            accel.x = __bswap_16(*buf++);
+            accel.y = __bswap_16(*buf++);
+            accel.z = __bswap_16(*buf++);
+            accel.t+= 0.01;
+            if( ++accel_latest >= ACCEL_LOG_SIZE)
+                accel_latest = 0;
+            accel_log[accel_latest] = accel;
+        }
+        //ESP_LOGI(TAG, "%d %d %d", accel.x, accel.y, accel.z);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 
     ESP_WARN(i2c_driver_delete(I2C_MASTER_NUM));
